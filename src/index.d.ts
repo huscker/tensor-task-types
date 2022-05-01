d77cb21a1c386fc168c68d9943b73ffdfe959acf
huscker <egostalker03@mail.ru>
2022-05-02 00:55:13 +0300

    done

--- src/index.d.ts
+++ src/index.d.ts
@@ -4,7 +4,7 @@
 - age - число
 - gender - 'male' или 'female'
  */
-export function getPersons();
+export function getPersons(): Array<{name: string, age:number, gender: 'male'|'female'}>;
 
 /*
 Типизируйте функцию personToString, возвращающую строку и принимающую объект одного из следующих форматов:
@@ -18,4 +18,4 @@ export function getPersons();
    - gender - 'male' или 'female'
    - company- строка
  */
-export function personToString(person);
+export function personToString(person: {name: string, age:number, gender: 'male'|'female', company?: string}) : string;
